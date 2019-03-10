488737c7db9fdd90fd4d8be2ee2ed847a51f4452
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-03-10 19:58:01 +0200

    Main task was fulfilled in Main.java file.

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 import com.queue.*;
 import com.exception.*;
 
 public class Main {
     public static void main(String[] args) {
-        MyQueue arrayQueue = new MyArrayQueue(); // Array queue
-        MyLinkedQueue linkedQueue = new MyLinkedQueue(); // Linked queue
-        MyPriorityQueue priorityQueue = new MyPriorityQueue(); // Priority queue
+        Scanner scan = new Scanner(System.in);
+        // MyQueue arrayQueue = new MyArrayQueue(); // Array queue
+        // MyLinkedQueue linkedQueue = new MyLinkedQueue(); // Linked queue
+        MyPriorityQueue V = new MyPriorityQueue(); // Priority queue
+        MyPriorityQueue S = new MyPriorityQueue(); // Priority queue
+        MyPriorityQueue R = new MyPriorityQueue(); // Priority queue
+
+        System.out.print("Input n: ");
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            V.push((int)(Math.random() * 10), i);
+            S.push((int)(Math.random() * 10), i);
+        }
+
+        System.out.println("V: " + V.toString());
+        System.out.println("S: " + S.toString());
+
+        int dob = 0;
+
+        for (int i = 0; i < n; i++) {
+            try {
+                dob += V.pop() * S.pop();
+            } catch (EmptyQueueException err) {
+                System.out.println(err.getMessage());
+                System.exit(1);
+            }
+        }
+
+        System.out.println("Result: " + dob);
     }
 }
